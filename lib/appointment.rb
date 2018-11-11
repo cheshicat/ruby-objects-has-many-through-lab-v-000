a9a867568492cd593db5ac7869eed185bf6cde49
Allison Howard <alliemhow@gmail.com>
2018-11-11 11:54:13 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,20 @@
+class Appointment
+  attr_accessor :patient, :doctor:, :date
+end
+
+
+
+
+attr_accessor :name, :artist, :genre
+@@all = []
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@all << self
+end
+
+def self.all
+  @@all
+end
