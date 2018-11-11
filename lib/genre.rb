c43cbcfe9b0b267d30aa352c1e3ff30ab0738522
Allison Howard <alliemhow@gmail.com>
2018-11-11 10:28:44 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -11,4 +11,16 @@ class Genre
     @@all
   end
 
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    Artist.all.select do |artist|
+      artist.genre == self
+    end
+  end
+
 end
